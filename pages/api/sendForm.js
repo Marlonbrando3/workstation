df5015363d4d3dd48e457ebc161b5c799f70ecfc
Marek <marek@macbook-air-marek.home>
2023-04-05 14:57:06 +0200

    „3”

--- pages/api/sendForm.js
+++ pages/api/sendForm.js
@@ -27,31 +27,31 @@ export default async (req, res) => {
     await transporter.verify(function (error, success) {
       if (error) {
           console.log(error);
+          res.status(400)
       } else {
           console.log("Server is ready to take our messages");
+          res.status(200)
       }})
 
 
-    const mailData = {
-      from: req.body.email,
-      to: 'www@work-station.pl',
-      subject: `Wiadomość ze strony od: ${req.body.name}`,
-      text: "Hello. This email is for your email verification.",
-      html: 
-      `Imię i nazwisko / firma: ${req.body.name}`+`<br>`+
-      `Email kontaktowy: ${req.body.email}`+`<br>`+
-      `Telefon kontaktowy: ${req.body.number}`+`<br><br>`+
-      `Wiadomość ${req.body.message}`+`<br>`
-    }
+    // const mailData = {
+    //   from: req.body.email,
+    //   to: 'www@work-station.pl',
+    //   subject: `Wiadomość ze strony od: ${req.body.name}`,
+    //   text: "Hello. This email is for your email verification.",
+    //   html: 
+    //   `Imię i nazwisko / firma: ${req.body.name}`+`<br>`+
+    //   `Email kontaktowy: ${req.body.email}`+`<br>`+
+    //   `Telefon kontaktowy: ${req.body.number}`+`<br><br>`+
+    //   `Wiadomość ${req.body.message}`+`<br>`
+    // }
 
-    await transporter.sendMail(mailData, function (err, info) {
-      console.log("wysyłam")
-      if(err)
-        console.log("błąd: " +err)
-      else
-        console.log("wysłane: "+info)
-    })
-    res.status(200)
-
+    // await transporter.sendMail(mailData, function (err, info) {
+    //   console.log("wysyłam")
+    //   if(err)
+    //     console.log("błąd: " +err)
+    //   else
+    //     console.log("wysłane: "+info)
+    // })
 
 }
