e81c6ec8be3c6af3db49e31e825235b6f2876cc6
Marek <marek@macbook-air-marek.home>
2023-04-05 15:15:08 +0200

    „3”

--- pages/api/sendForm.js
+++ pages/api/sendForm.js
@@ -2,13 +2,6 @@
 const nodemailer = require('nodemailer')
 
 export default async (req, res) => {
-
-  res.status(200).json({ 
-    name: req.body.name,
-    number: req.body.number,
-    email: req.body.email,
-    message: req.body.message
-  })
   
     // create reusable transporter object using the default SMTP transport
     let transporter = await nodemailer.createTransport({
@@ -27,10 +20,10 @@ export default async (req, res) => {
     await transporter.verify(function (error, success) {
       if (error) {
           console.log(error);
-          res.status(400)
+          return res.json({info:"nie działa bo: "+error})
       } else {
           console.log("Server is ready to take our messages");
-          res.status(400)
+          return res.json({info:"działa"})
       }})
 
 
