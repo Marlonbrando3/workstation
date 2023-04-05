f751c08e6be754215ad307d317bb55bbe0169d8f
Marek <marek@macbook-air-marek.home>
2023-04-05 15:19:52 +0200

    „3”

--- pages/api/sendForm.js
+++ pages/api/sendForm.js
@@ -20,31 +20,34 @@ export default async (req, res) => {
     await transporter.verify(function (error, success) {
       if (error) {
           console.log(error);
-          return res.json({info:"nie działa bo: "+error})
+          // return res.json({info:"nie działa bo: "+error})
       } else {
           console.log("Server is ready to take our messages");
-          return res.json({info:"działa: "+success})
+          // return res.json({info:"działa: "+success})
       }})
 
 
-    // const mailData = {
-    //   from: req.body.email,
-    //   to: 'www@work-station.pl',
-    //   subject: `Wiadomość ze strony od: ${req.body.name}`,
-    //   text: "Hello. This email is for your email verification.",
-    //   html: 
-    //   `Imię i nazwisko / firma: ${req.body.name}`+`<br>`+
-    //   `Email kontaktowy: ${req.body.email}`+`<br>`+
-    //   `Telefon kontaktowy: ${req.body.number}`+`<br><br>`+
-    //   `Wiadomość ${req.body.message}`+`<br>`
-    // }
+    const mailData = {
+      from: req.body.email,
+      to: 'www@work-station.pl',
+      subject: `Wiadomość ze strony od: ${req.body.name}`,
+      text: "Hello. This email is for your email verification.",
+      html: 
+      `Imię i nazwisko / firma: ${req.body.name}`+`<br>`+
+      `Email kontaktowy: ${req.body.email}`+`<br>`+
+      `Telefon kontaktowy: ${req.body.number}`+`<br><br>`+
+      `Wiadomość ${req.body.message}`+`<br>`
+    }
 
-    // await transporter.sendMail(mailData, function (err, info) {
-    //   console.log("wysyłam")
-    //   if(err)
-    //     console.log("błąd: " +err)
-    //   else
-    //     console.log("wysłane: "+info)
-    // })
+    await transporter.sendMail(mailData, function (err, info) {
+      console.log("wysyłam")
+      if(err){
+        console.log("błąd: " +err)
+        return res.json({info:"nie działa bo: "+err})
+      }
+      else{
+        console.log("wysłane: "+info)
+        return res.json({info:"działa: "+info})
+    }})
 
 }
